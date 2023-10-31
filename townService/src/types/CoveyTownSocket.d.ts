90665680433b66937dde3c2df50624f4a9ac8fe5
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-10-30 21:18:45 -0400

    Work on fleshing out types with commentary

--- townService/src/types/CoveyTownSocket.d.ts
+++ townService/src/types/CoveyTownSocket.d.ts
@@ -15,3 +15,70 @@ export type SocketData = Record<string, never>;
 export type CoveyTownSocket = Socket<ClientToServerEvents, ServerToClientEvents>;
 export type TownEmitter = BroadcastOperator<ServerToClientEvents, SocketData>;
 export type TownEmitterFactory = (townID: string) => TownEmitter;
+
+export interface StopMotionGameState {
+    // Who is working the animation software?
+    // The software is not co-op, so there is a unique animator.
+    // That would require us to resolve race conditions etc.
+    animator: PlayerID;
+
+    // Who is viewing the created animations?
+    // Multiple readers of data does not produce a race condition, so 
+    // there can be many spectators.
+    spectators: PlayerID[]
+}
+
+export interface Animation {
+
+    // The list of frames of the animation.
+    frames: Frame[]
+}
+
+export interface Frame {
+    // TODO: background_image
+
+    // The list of figures of the animation.
+    figures: FigureElement[]
+}
+
+/**
+ * Represents an individual limb on a figure.
+ * These are trees, so one root FigureElement is the representative of 
+ * an entire figure.
+ */
+export interface FigureElement {
+
+    // Is this a root?
+    // A typical root for a stick figure is the torso.
+    isRoot: boolean
+
+    // TODO: We need to store information about
+    // the actual visual representation of this limb.
+    // One way to do it is to bake it into this FigureElement.
+    // The next question is, what representation should be used?
+    // In any case, that representation needs to connect to x, y, and rotation_degrees
+    // To be rendered correctly on the animation canvas.
+
+    // The x position on the canvas in pixels
+    x: number
+
+    // The y position on the canvas in pixels
+    y: number
+
+    // The rotation of the figure element in degrees.
+    rotation_degrees: number
+    children: FigureElement[]
+}
+
+/**
+ * It would be complex to have the backend parse the actual raw clicks given to the frontend.
+ * As such, a StopMotionMove consists of an update to the animation state.
+ */
+export interface StopMotionMove {
+  // What frame was updated?
+  frame_index: int
+
+  // We do not use a delta-based encoding yet, since that's more complex.
+  // Just send over the whole new frame to apply.
+  frame_update: Frame
+}
