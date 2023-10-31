8172b2628bfa057c39c8b12306ac364577ce6aee
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-10-30 20:57:29 -0400

    StopMotionGame skeleton

---
+++ townService/src/town/games/StopMotionGame.ts
@@ -0,0 +1,16 @@
+import Game from './Game';
+import { GameMove, TicTacToeGameState, TicTacToeMove } from '../../types/CoveyTownSocket';
+
+export default class StopMotionGame extends Game<StopMotionGameState, StopMotionMove> {
+    public applyMove() {
+
+    }
+
+    protected _join() {
+
+    }
+
+    protected _leave() {
+    
+    }
+}
