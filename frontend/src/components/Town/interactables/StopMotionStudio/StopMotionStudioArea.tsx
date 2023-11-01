244f79eb0407363829fe7b4c2de60f236eade672
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-10-31 22:02:32 -0400

    Konva dragndrop

--- frontend/src/components/Town/interactables/StopMotionStudio/StopMotionStudioArea.tsx
+++ frontend/src/components/Town/interactables/StopMotionStudio/StopMotionStudioArea.tsx
@@ -14,9 +14,10 @@ import useTownController from '../../../../hooks/useTownController';
 import { InteractableID } from '../../../../types/CoveyTownSocket';
 // import StopMotionArea from '../StopMotionArea';
 import StopMotionAreaInteractable from '../StopMotionArea';
-import { Stage, Layer, Rect, Text } from 'react-konva';
 import Konva from 'konva'; 
 import StopMotionAreaController from '../../../../classes/interactable/StopMotionAreaController';
+import { Stage, Layer, Star, Text } from 'react-konva'; 
+
 
 const ColoredRect = () => {
   const [color, setColor] = useState('green');
@@ -38,10 +39,52 @@ const ColoredRect = () => {
   );
 }; 
 
+function generateShapes() {
+  return [...Array(10)].map((_, i) => ({
+    id: i.toString(),
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    rotation: Math.random() * 180,
+    isDragging: false,
+  }));
+}
+
+
 function StopMotionStudioArea({ interactableID }: { interactableID: InteractableID }): JSX.Element {
   //   const stopMotionAreaController =
   //     useInteractableAreaController<StopMotionAreaController>(interactableID);
   //   const townController = useTownController();
+
+  const [stars, setStars] = React.useState(generateShapes());
+
+  // To implement animation of figures,
+  // 1: Keep every FigureElement in a list here, map as necessary.
+  // 2: If the FigureELement is not the root, do a polar motion.
+  // 3: In any event, intercept the handleDrag{Start,End}.
+
+  const handleDragStart = (e) => {
+    const id = e.target.id();
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: star.id === id,
+        };
+      })
+    );
+  };
+
+  const handleDragEnd = (e) => {
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: false,
+        };
+      })
+    );
+  };
+
 
   useEffect(() => {
     console.log('exists');
@@ -49,12 +92,35 @@ function StopMotionStudioArea({ interactableID }: { interactableID: Interactable
 
   return (
     <Container>
-    <Stage width={window.innerWidth} height={window.innerHeight}>
+<Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
-        <Text text="Try click on rect" />
-        <ColoredRect />
+        <Text text="Try to drag a star" />
+        {stars.map((star) => (
+          <Star
+            key={star.id}
+            id={star.id}
+            x={star.x}
+            y={star.y}
+            numPoints={5}
+            innerRadius={20}
+            outerRadius={40}
+            fill="#89b717"
+            opacity={0.8}
+            draggable
+            rotation={star.rotation}
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            shadowOffsetX={star.isDragging ? 10 : 5}
+            shadowOffsetY={star.isDragging ? 10 : 5}
+            scaleX={star.isDragging ? 1.2 : 1}
+            scaleY={star.isDragging ? 1.2 : 1}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          />
+        ))}
       </Layer>
-    </Stage> 
+    </Stage>
       <Button colorScheme='blue'>Button</Button>
     </Container>
   );
