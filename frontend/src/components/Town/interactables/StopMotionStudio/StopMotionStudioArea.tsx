3dc6f2b625bcaec32e17b17a5d9a6290fa3da995
Cam Powell <powell.c@northeastern.edu>
2023-11-14 18:44:31 -0500

    frame save tentatively working, drag broken

--- frontend/src/components/Town/interactables/StopMotionStudio/StopMotionStudioArea.tsx
+++ frontend/src/components/Town/interactables/StopMotionStudio/StopMotionStudioArea.tsx
@@ -39,10 +39,159 @@ function StopMotionStudioArea({ interactableID }: { interactableID: Interactable
     );
   };
 
-  function addNewFrame() {}
+  type CanvasProps = {
+    frames: Frame[];
+    setFrames: React.Dispatch<React.SetStateAction<Frame[]>>;
+  };
+  const figure1Torso: FigureElement = {
+    type: 'figure',
+    // a KonvaRect
+    appearance: {
+      type: 'rect',
+      length: 50,
+      width: 20,
+    },
+    id: 'figure_1_torso',
+    // This is the root
+    parent: undefined,
+    // Because this is the root, these are absolute posns
+    offset_x: 773, //----------------------------------------------------------> these offset x and y should probably not be hard coded
+    offset_y: 521,
+    offset_rotation: 0,
+    offset_attach_rotation: 0,
+    offset_attach_x: 0,
+    offset_attach_y: 0,
+    isDragging: false,
+  };
+
+  const figure1Head: FigureElement = {
+    type: 'figure',
+    // a KonvaCircle
+    appearance: {
+      type: 'circle',
+      radius: 10,
+    },
+    id: 'figure_1_head',
+    parent: figure1Torso,
+    offset_x: 10,
+    offset_y: -10,
+    offset_rotation: 0,
+    offset_attach_rotation: Math.PI / 2,
+    offset_attach_x: 0,
+    offset_attach_y: 10,
+    isDragging: false,
+  };
+
+  const figure1LeftLeg: FigureElement = {
+    type: 'figure',
+    appearance: {
+      type: 'rect',
+      length: 25,
+      width: 5,
+    },
+    id: 'figure_1_left_leg',
+    parent: figure1Torso,
+    offset_x: 0,
+    offset_y: 45,
+    // for now
+    offset_rotation: 0,
+    offset_attach_rotation: -(Math.PI / 2),
+    offset_attach_x: 0,
+    offset_attach_y: 0,
+    isDragging: false,
+  };
+
+  const frame1: Frame = {
+    frameID: 1,
+    canvasElements: [figure1Head, figure1LeftLeg, figure1Torso],
+  };
+
+  const figure2Torso: FigureElement = {
+    type: 'figure',
+    // a KonvaRect
+    appearance: {
+      type: 'rect',
+      length: 50,
+      width: 20,
+    },
+    id: 'figure_1_torso',
+    // This is the root
+    parent: undefined,
+    // Because this is the root, these are absolute posns
+    offset_x: 773, //----------------------------------------------------------> these offset x and y should probably not be hard coded
+    offset_y: 521,
+    offset_rotation: 0,
+    offset_attach_rotation: 0,
+    offset_attach_x: 0,
+    offset_attach_y: 0,
+    isDragging: false,
+  };
+
+  const figure2Head: FigureElement = {
+    type: 'figure',
+    // a KonvaCircle
+    appearance: {
+      type: 'circle',
+      radius: 10,
+    },
+    id: 'figure_1_head',
+    parent: figure2Torso,
+    offset_x: 10,
+    offset_y: -10,
+    offset_rotation: 0,
+    offset_attach_rotation: Math.PI / 2,
+    offset_attach_x: 0,
+    offset_attach_y: 10,
+    isDragging: false,
+  };
+
+  const figure2LeftLeg: FigureElement = {
+    type: 'figure',
+    appearance: {
+      type: 'rect',
+      length: 25,
+      width: 5,
+    },
+    id: 'figure_1_left_leg',
+    parent: figure2Torso,
+    offset_x: 0,
+    offset_y: 45,
+    // for now
+    offset_rotation: 0,
+    offset_attach_rotation: -(Math.PI / 2),
+    offset_attach_x: 0,
+    offset_attach_y: 0,
+    isDragging: false,
+  };
+
+  const frame2: Frame = {
+    frameID: 2,
+    canvasElements: [figure2Head, figure2Torso],
+  };
+
+  const [frames, setFrames] = useState<Frame[]>([frame1, frame2]);
+
+  function addNewFrame() {
+    setFrames((prevFrames: Frame[]) => {
+      // Clone the last frame's elements to create a new frame
+      const newFrameElements = prevFrames[prevFrames.length - 1].canvasElements.map(elem => {
+        return { ...elem }; // Shallow copy each element
+      });
+
+      // Create a new frame with current elements and new ID
+      const newFrame = {
+        frameID: prevFrames.length + 1,
+        canvasElements: newFrameElements,
+      };
+
+      //const newFrameList = [...prevFrames, newFrame];
+
+      return [...prevFrames, newFrame]; //add new frame
+    });
+  }
 
   // the interactable canvas to construct the stop motion scenes
-  const Canvas = () => {
+  const Canvas: React.FC<CanvasProps> = ({ setFrames }) => {
     // the canvas should always be displaying two screens
     // 1. past frame which is not interactable
     // 2. current editable frame with full opacity on top
@@ -52,150 +201,24 @@ function StopMotionStudioArea({ interactableID }: { interactableID: Interactable
     const canvasWidth = 1300;
     const canvasHeight = 800;
 
-    const figure1Torso: FigureElement = {
-      type: 'figure',
-      // a KonvaRect
-      appearance: {
-        type: 'rect',
-        length: 50,
-        width: 20,
-      },
-      id: 'figure_1_torso',
-      // This is the root
-      parent: undefined,
-      // Because this is the root, these are absolute posns
-      offset_x: 773, //----------------------------------------------------------> these offset x and y should probably not be hard coded
-      offset_y: 521,
-      offset_rotation: 0,
-      offset_attach_rotation: 0,
-      offset_attach_x: 0,
-      offset_attach_y: 0,
-      isDragging: false,
-    };
-
-    const figure1Head: FigureElement = {
-      type: 'figure',
-      // a KonvaCircle
-      appearance: {
-        type: 'circle',
-        radius: 10,
-      },
-      id: 'figure_1_head',
-      parent: figure1Torso,
-      offset_x: 10,
-      offset_y: -10,
-      offset_rotation: 0,
-      offset_attach_rotation: Math.PI / 2,
-      offset_attach_x: 0,
-      offset_attach_y: 10,
-      isDragging: false,
-    };
-
-    const figure1LeftLeg: FigureElement = {
-      type: 'figure',
-      appearance: {
-        type: 'rect',
-        length: 25,
-        width: 5,
-      },
-      id: 'figure_1_left_leg',
-      parent: figure1Torso,
-      offset_x: 0,
-      offset_y: 45,
-      // for now
-      offset_rotation: 0,
-      offset_attach_rotation: -(Math.PI / 2),
-      offset_attach_x: 0,
-      offset_attach_y: 0,
-      isDragging: false,
-    };
-
-    const frame1: Frame = {
-      frameID: 1,
-      canvasElements: [figure1Head, figure1LeftLeg, figure1Torso],
-    };
-
-    const figure2Torso: FigureElement = {
-      type: 'figure',
-      // a KonvaRect
-      appearance: {
-        type: 'rect',
-        length: 50,
-        width: 20,
-      },
-      id: 'figure_1_torso',
-      // This is the root
-      parent: undefined,
-      // Because this is the root, these are absolute posns
-      offset_x: 773, //----------------------------------------------------------> these offset x and y should probably not be hard coded
-      offset_y: 521,
-      offset_rotation: 0,
-      offset_attach_rotation: 0,
-      offset_attach_x: 0,
-      offset_attach_y: 0,
-      isDragging: false,
-    };
-
-    const figure2Head: FigureElement = {
-      type: 'figure',
-      // a KonvaCircle
-      appearance: {
-        type: 'circle',
-        radius: 10,
-      },
-      id: 'figure_1_head',
-      parent: figure2Torso,
-      offset_x: 10,
-      offset_y: -10,
-      offset_rotation: 0,
-      offset_attach_rotation: Math.PI / 2,
-      offset_attach_x: 0,
-      offset_attach_y: 10,
-      isDragging: false,
-    };
-
-    const figure2LeftLeg: FigureElement = {
-      type: 'figure',
-      appearance: {
-        type: 'rect',
-        length: 25,
-        width: 5,
-      },
-      id: 'figure_1_left_leg',
-      parent: figure2Torso,
-      offset_x: 0,
-      offset_y: 45,
-      // for now
-      offset_rotation: 0,
-      offset_attach_rotation: -(Math.PI / 2),
-      offset_attach_x: 0,
-      offset_attach_y: 0,
-      isDragging: false,
-    };
-
-    const frame2: Frame = {
-      frameID: 2,
-      canvasElements: [figure2Head, figure2LeftLeg, figure2Torso],
-    };
-
-    // stores the canvas frames
-    const [frames, setFrames] = useState<Frame[]>([frame1, frame2]);
-
-    // this use effect currently manually sets one frame for testing
-    useEffect(() => {}, []);
-
-    // updater callback for current frame elements
     function updateFrameElements(elems: CanvasElement[]) {
-      setFrames(prevFrames => {
+      setFrames((prevFrames: Frame[]) => {
         // Make a shallow copy of the previous frames
         const updatedFrames = [...prevFrames];
         // Update the last frame (assuming there is at least one frame)
         const lastFrame = updatedFrames[updatedFrames.length - 1];
         lastFrame.canvasElements = elems;
-
+        console.log('updated frame elements');
         return updatedFrames;
       });
     }
+    // stores the canvas frames
+    //const [frames, setFrames] = useState<Frame[]>([frame1, frame2]);
+
+    // this use effect currently manually sets one frame for testing
+    useEffect(() => {}, []);
+
+    // updater callback for current frame elements
 
     return (
       <Box
@@ -207,27 +230,31 @@ function StopMotionStudioArea({ interactableID }: { interactableID: Interactable
         }}>
         <Stage width={canvasWidth} height={canvasHeight}>
           {/* previous layer (non interactable) */}
-          <Layer opacity={0.1}>
-            {frames[frames.length - 2]
-              ? frames[frames.length - 2].canvasElements.map(elem => {
-                  if (elem.type == 'figure') {
-                    const figureElem = elem as FigureElement; // case current element to figure element
-                    return toKonvaElement(
-                      figureElem,
-                      frames[frames.length - 1].canvasElements,
-                      updateFrameElements,
-                      false,
-                    );
-                  } else if (elem.type == 'simpleShape') {
-                    // return some other type here
-                    return {};
-                  }
-                })
-              : undefined}
-          </Layer>
-          {/* interactable layer (interactable) */}
+          {/* Render the second-to-last frame with lower opacity */}
+          {frames.length > 1 && (
+            <Layer opacity={0.1}>
+              {frames[frames.length - 2].canvasElements.map(elem => {
+                // Render each element of the second-to-last frame
+                if (elem.type == 'figure') {
+                  const figureElem = elem as FigureElement; // case current element to figure element
+                  return toKonvaElement(
+                    figureElem,
+                    frames[frames.length - 1].canvasElements,
+                    updateFrameElements,
+                    false,
+                  );
+                } else if (elem.type == 'simpleShape') {
+                  // return some other type here
+                  return {};
+                }
+              })}
+            </Layer>
+          )}
+
+          {/* Render the last frame (current frame) */}
           <Layer>
             {frames[frames.length - 1].canvasElements.map(elem => {
+              // Render each element of the last frame (current frame)
               if (elem.type == 'figure') {
                 const figureElem = elem as FigureElement; // case current element to figure element
                 return toKonvaElement(
@@ -247,8 +274,12 @@ function StopMotionStudioArea({ interactableID }: { interactableID: Interactable
     );
   };
 
+  type ControlPanelProps = {
+    addNewFrame: () => void;
+  };
+
   // Bottom controll panel for progressing through and viewing animation
-  const ControlPanel = () => {
+  const ControlPanel: React.FC<ControlPanelProps> = ({ addNewFrame: addFrame }) => {
     return (
       <Box display='flex' alignItems='center' justifyContent='center' width={'100%'}>
         <Flex direction={'row'} justifyContent={'space-between'} padding={'10px'} width={'80%'}>
@@ -270,7 +301,7 @@ function StopMotionStudioArea({ interactableID }: { interactableID: Interactable
             </Button>
           </Box>
 
-          <Button size='md' height='48px' onClick={addNewFrame}>
+          <Button size='md' height='48px' onClick={addFrame}>
             Add Latest Frame
           </Button>
 
@@ -297,11 +328,11 @@ function StopMotionStudioArea({ interactableID }: { interactableID: Interactable
           <Spacer />
 
           {/* canvas for creating stop motion scene */}
-          <Canvas />
+          <Canvas frames={frames} setFrames={setFrames} />
         </Flex>
 
         {/* items in row two */}
-        <ControlPanel />
+        <ControlPanel addNewFrame={addNewFrame} />
       </Flex>
     </Box>
   );
