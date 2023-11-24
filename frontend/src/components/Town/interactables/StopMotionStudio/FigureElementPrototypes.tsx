09bd7ee9df22fa20a9eeef1e29104e7fdc23b02d
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-11-24 17:45:20 -0500

    Bird wings not rotatable

--- frontend/src/components/Town/interactables/StopMotionStudio/FigureElementPrototypes.tsx
+++ frontend/src/components/Town/interactables/StopMotionStudio/FigureElementPrototypes.tsx
@@ -289,6 +289,7 @@ const birdFigureWing1: FigureElement = {
   offset_attach_x: -3,
   offset_attach_y: 0,
   isDragging: false,
+  dragOverride: true
 };
 
 const birdFigureWing2: FigureElement = {
@@ -309,6 +310,7 @@ const birdFigureWing2: FigureElement = {
   offset_attach_x: +3,
   offset_attach_y: 0,
   isDragging: false,
+  dragOverride: true
 };
 
 export const PERSON_FIGURE_PROTO = [
