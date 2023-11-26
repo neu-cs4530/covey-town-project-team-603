85d4c77c8bac1bc0e80050e593666674a03bd588
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-11-26 14:33:52 -0500

    Fixed merge problem: was downloading twice

--- frontend/src/components/Town/interactables/StopMotionStudio/StopMotionEditor.tsx
+++ frontend/src/components/Town/interactables/StopMotionStudio/StopMotionEditor.tsx
@@ -197,18 +197,6 @@ export function StopMotionEditor({ backHome }: { backHome: () => void }): JSX.El
     const stranim = JSON.stringify(frames);
     const mimetype = 'application/json';
     const blob = new Blob([stranim], { type: mimetype });
-    const bloburl = URL.createObjectURL(blob);
-
-    const a = document.createElement('a');
-    document.body.appendChild(a);
-    a.style.cssText = 'display: none';
-    a.href = bloburl;
-
-    a.download = 'animation.json';
-    a.click();
-
-    URL.revokeObjectURL(bloburl);
-    document.body.removeChild(a);
     saveBlob(blob);
   }
 
